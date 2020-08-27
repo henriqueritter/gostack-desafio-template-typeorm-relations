8fd363bb4ec21995db992d84ec8877b45b0e816c
Henrique Ritter <henrique-ritter@hotmail.com>
2020-08-27 15:46:54 -0300

    Challenge done

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -3,9 +3,21 @@ import { Request, Response } from 'express';
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
 import { container } from 'tsyringe';
+import CustomersRepository from '../../typeorm/repositories/CustomersRepository';
 
 export default class CustomersController {
+  // constructor();
+
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createCustomer = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer);
   }
 }
